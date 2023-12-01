b2c92652aa224cb5ebf97eda9f5e7d6c74ffb987
Rhys Shaw <rhysc3p0@gmail.com>
2023-12-01 15:48:11 +0000

    solution clean up

--- 2023D1/main.cpp
+++ 2023D1/main.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
 #include <algorithm>
 
 
+// function to read the lines from a file and return them as a vector of strings
 
 std::vector<std::string> readLines(std::string path){
 
-    std::vector<std::string> lines; // Vector to store the lines from the file.
-    std::ifstream inputFile(path); // Create an input file stream object
-    std::string line; // Read the file line by line
+    std::vector<std::string> lines;         // Vector to store the lines from the file.
+    std::ifstream inputFile(path);          // Create an input file stream object
+    std::string line;                       // Read the file line by line
 
-    while (std::getline(inputFile, line)){ // while there is a line to read
-        lines.push_back(line); // Add the line to the vector
+    while (std::getline(inputFile, line)){  // while there is a line to read
+        lines.push_back(line);              // Add the line to the vector
     }
     
-    inputFile.close(); // Close the file
+    inputFile.close();                      // Close the file
     return lines;
 }
 
@@ -25,33 +25,45 @@ std::vector<std::string> readLines(std::string path){
 
 int main(int argc, char const *argv[]){
     
-    
-    std::vector<std::string> lines = readLines("calibration_data.txt"); // Read the lines from the file using the function
-    
-    // print the lines from the vector to the console
-    for (std::string line : lines){
-        std::cout << line << std::endl;
+    if(argc < 2) {
+        std::cerr << "ERROR!\n";
+        std::cerr << "No file path provided. Please provide a file path as an argument.\n";
+        return 1;
     }
+    
+    std::string file_path = argv[1];
+    // print the file path
+    std::cout << "File Path: " << file_path << std::endl;
 
+    std::vector<std::string> lines = readLines(file_path); // Read the lines from the file using the function
+    
 
     // correct for speltout words.
 
     std::vector<std::string> numberStringss = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     
 
-    //loop through each line and add the digit to the location of the numberString in the line + 1.
+    // loop through each line and add the digit to the location of the numberString in the line + 1.
+    // replaces onetwo3four with one1two2three3four4.
 
     for (std::string& line : lines) {
+
         // Convert any numberStrings to digits and do the change after the loop.
+        
         for (const std::string& numberString : numberStringss) {
+        
             // while loop to replace all instances of the numberString in the line.
+        
             while (line.find(numberString) != std::string::npos) {
+        
                 // Find the position of the numberString in the line.
+        
                 int position = line.find(numberString);
+        
                 // add the digit to the location of the numberString in the line + 1.
+        
                 line.insert(position+1, std::to_string(std::distance(numberStringss.begin(), std::find(numberStringss.begin(), numberStringss.end(), numberString)) + 1));
-                // remove the numberString from the line.
-                //line.erase(position, numberString.length());
+    
             }
 
         }   
@@ -60,54 +72,56 @@ int main(int argc, char const *argv[]){
 
     // go though each line and look for the first and last number string 
 
+    // create two vectors to store the first and last number strings
+
     std::vector<std::string> firstNumberStringvector;
     std::vector<std::string> lastNumberStringvector;
 
 
     // loop through each line
+    
     for (std::string line : lines){
+    
         // loop through each character in the line  
         
         for (char character : line){
+    
             // if the character is a number
             if (isdigit(character)){
+    
                 // save the character as a string and push it into the first number string vector
                 std::string numberString(1, character);
+    
                 firstNumberStringvector.push_back(numberString);
+    
                 int position = line.find(numberString);
+    
                 break; // break out of the loop since we want the first number
             }
         }
 
         //loop though each character in the line backwards
         for (int i = line.length() - 1; i >= 0; i--){
+        
             // if the character is a number
+        
             if (isdigit(line[i])){
+        
                 // save the character as a string and push it into the last number string vector
+        
                 std::string numberString(1, line[i]);
+        
                 lastNumberStringvector.push_back(numberString);
-                break; // break out of the loop since we want the last number
+        
+                break;                          // break out of the loop since we want the last number
             }
-
         }
     }
-
-
-
     
-    std::cout << "First Number String: ";
-    for (std::string numberString : firstNumberStringvector){
-        std::cout << numberString;
-    }
-    std::cout << std::endl;
-    std::cout << "Last Number String: ";
-    for (std::string numberstring : lastNumberStringvector){
-        std::cout << numberstring;
-    }
-
     // append the strings of the same index in the vectors together into a new vector
-    
+
     std::vector<std::string> numberStrings;
+
     for (int i = 0; i < firstNumberStringvector.size(); i++){
         numberStrings.push_back(firstNumberStringvector[i] + lastNumberStringvector[i]);
     }
@@ -117,20 +131,21 @@ int main(int argc, char const *argv[]){
     // print the number strings
     std::cout << std::endl;
     std::cout << "Number Strings: ";
+    
     for (std::string numberString : numberStrings){
         std::cout << numberString << " ";
     }
+    
     std::cout << std::endl;
 
     // print the sum of the numbers in the vector
     int sum = 0;
     for (std::string numberString : numberStrings){
-        sum += std::stoi(numberString);
+        sum += std::stoi(numberString);             // convert the string to an int and add it to the sum
     }
 
-    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Sum: " << sum << std::endl;       // print the sum
     
 
     return 0;
-
 }
--- 2023D1/run.sh
+++ 2023D1/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cpp_file="main.cpp"
 output_executable="main.exe"
+file_name="part2_data.txt"
 g++ -o "$output_executable" "$cpp_file" -std=c++11
 if [ $? -eq 0 ]; then
   echo "Compilation successful. Executable: ./$output_executable"
@@ -8,4 +9,4 @@ else
   echo "Compilation failed."
 fi
 
-./"$output_executable"
+./"$output_executable" "$file_name"
